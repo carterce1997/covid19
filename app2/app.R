1a901d639deebf602ad9f8648e9a9666609e48ee
Chris Carter <carterce1997@gmail.com>
2020-03-31 13:54:17 -0400

    app2

--- app2/app.R
+++ app2/app.R
@@ -5,7 +5,11 @@ library(tidyverse)
 get_covid_data <- function() {
   
   results <-
-    vroom::vroom('https://covidtracking.com/api/states.csv') 
+    vroom::vroom('https://covidtracking.com/api/states/daily.csv') %>% 
+    mutate(date = ymd(date)) %>% 
+    replace(is.na(.), 0)
+  
+  return(results)
   
 }
 
@@ -19,7 +23,7 @@ cumulative_chart <- function(covid_data, state_, variable, logscale) {
     ggplot() +
     geom_line(aes(x = date, y = !!variable)) +
     { if (logscale) scale_y_log10() } +
-    ggthemes::theme_hc()
+    theme_minimal()
   
 }
 
@@ -32,37 +36,53 @@ daily_chart <- function(covid_data, state_, variable) {
     filter(state == state_) %>% 
     ggplot() +
     geom_bar(aes(x = date, y = !!variable), stat = 'identity') +
-    ggthemes::theme_hc()
+    theme_minimal()
   
 }
 
+plot_height <- '300px'
+
 ui <- fluidPage(
+  titlePanel('COVID-19 Dashboard'),
   sidebarLayout(
     sidebarPanel(
-      selectizeInput('state', 'State', choices = sort(unique(get_covid_data()$state)), selected = 'USA'),
-      checkboxInput('logscale', 'Log Scale', value = FALSE)
+      selectizeInput('state', 'State', choices = sort(unique(get_covid_data()$state)), selected = 'NY'),
+      checkboxInput('logscale', 'Log Scale', value = FALSE),
+      dateRangeInput('daterange', 'Date Range', start = Sys.Date() - 14, end = Sys.Date()),
+      width = 2
     ),
     mainPanel(
       fluidRow(
         column(
           3,
-          plotOutput('total'),
-          plotOutput('positive'),
-          plotOutput('hospitalized'),
-          plotOutput('death')
+          h2('Tests'),
+          plotOutput('total', height = plot_height),
+          plotOutput('total_daily', height = plot_height)
         ),
         column(
           3,
-          plotOutput('total_daily'),
-          plotOutput('positive_daily'),
-          plotOutput('hospitalized_daily'),
-          plotOutput('death_daily')
+          h2('Cases'),
+          plotOutput('positive', height = plot_height),
+          plotOutput('positive_daily', height = plot_height)
+        ),
+        # column(
+        #   2,
+        #   plotOutput('hospitalized', height = plot_height),
+        #   plotOutput('hospitalized_daily', height = plot_height)
+        # ),
+        column(
+          3,
+          h2('Deaths'),
+          plotOutput('death', height = plot_height),
+          plotOutput('death_daily', height = plot_height)
         ),
         column(
-          6,
-          plotOutput('phase')
+          3,
+          h2('Phase Plot'),
+          plotOutput('phase', height = '600px')
         )
-      )
+      ),
+      width = 10
     )
   )
 )
@@ -73,67 +93,79 @@ server <- function(input, output, session) {
     
     invalidateLater(5 * 60 * 1000)
     
-    get_covid_data()
+    get_covid_data() %>% 
+      filter(date <= input$daterange[2], date >= input$daterange[1])
     
   })
   
-  total <- plotOutput({
+  output$total <- renderPlot({
     
     covid_data() %>% 
       cumulative_chart(input$state, total, input$logscale)
     
   })
   
-  positive <- plotOutput({
+  output$positive <- renderPlot({
     
+
     covid_data() %>% 
       cumulative_chart(input$state, positive, input$logscale)
     
   })  
   
-  hospitalized <- plotOutput({
+  output$hospitalized <- renderPlot({
     
     covid_data() %>% 
       cumulative_chart(input$state, hospitalized, input$logscale)
     
   })  
   
-  death <- plotOutput({
+  output$death <- renderPlot({
     
     covid_data() %>% 
       cumulative_chart(input$state, death, input$logscale)
     
   })  
   
-  total_daily <- plotOutput({
+  output$total_daily <- renderPlot({
     
     covid_data() %>% 
       daily_chart(input$state, totalTestResultsIncrease)
     
   })
   
-  positive_daily <- plotOutput({
+  output$positive_daily <- renderPlot({
     
     covid_data() %>% 
       daily_chart(input$state, positiveIncrease)
     
   })  
   
-  hospitalized_daily <- plotOutput({
+  output$hospitalized_daily <- renderPlot({
     
     covid_data() %>% 
       daily_chart(input$state, hospitalizedIncrease)
     
   })  
   
-  death_daily <- plotOutput({
+  output$death_daily <- renderPlot({
     
     covid_data() %>% 
       daily_chart(input$state, deathIncrease)
     
   })  
   
-  phase <- plotOutput({
+  output$phase <- renderPlot({
+    
+    covid_data() %>% 
+      filter(positive > 50, positiveIncrease > 0) %>% 
+      ggplot() +
+      geom_line(aes(x = positive, y = positiveIncrease, group = state, alpha = state == input$state)) +
+      scale_alpha_manual(values = c('TRUE' = 1, 'FALSE' = .1)) +
+      scale_x_log10() +
+      scale_y_log10() +
+      theme_minimal() +
+      theme(legend.position = 'none')
     
   })
   
